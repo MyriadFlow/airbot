f194b7a4f97258a7cf032fb3b3ff7b4fec9f7584
inciner8r <punarvpawade17@gmail.com>
2023-05-11 16:12:49 +0530

    add functions upscale, upscalemax and variation

--- app/bot/bot.go
+++ app/bot/bot.go
@@ -1,13 +1,17 @@
 package bot
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/MyriadFlow/airbot/app/handlers"
 	"github.com/bwmarrin/discordgo"
 )
 
-func BotInit() *discordgo.Session {
+func newBot() *discordgo.Session {
 	bot_token := os.Getenv("BOT_TOKEN")
 
 	sess, err := discordgo.New("Bot " + bot_token)
@@ -15,4 +19,28 @@ func BotInit() *discordgo.Session {
 		log.Fatal(err)
 	}
 	return sess
+}
+
+func BotInit() {
+	sess := newBot()
+
+	// Add all handlers
+	handlers.AddHandlers(sess)
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err := sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	sess.Close()
+
 }
--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -12,82 +12,192 @@ import (
 )
 
 func AddHandlers(sess *discordgo.Session) {
+	sess.AddHandler(generateImage)
+	// sess.AddHandler(upscale)
+	// sess.AddHandler(maxUpscale)
+	// sess.AddHandler(variation)
+}
 
-	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		url := "https://discord.com/api/v9/interactions"
-		server_id := os.Getenv("SERVER_ID")
-		user_token := os.Getenv("USER_TOKEN")
-		channel_id := os.Getenv("CHANNEL_ID")
+func generateImage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	url := "https://discord.com/api/v9/interactions"
+	server_id := os.Getenv("SERVER_ID")
+	user_token := os.Getenv("USER_TOKEN")
+	channel_id := os.Getenv("CHANNEL_ID")
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	prefix := "!mj"
+	args := strings.Split(m.Content, " ")
 
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-		prefix := "!mj"
-		args := strings.Split(m.Content, " ")
-
-		if args[0] != prefix {
-			return
-		}
-		if args[1] == "imagine" {
-			prompt := "Batman"
-			jsonStr := `{
-				"type": 2,
-				"application_id": "936929561302675456",
-				"guild_id": "` + server_id + `",
-				"channel_id": "` + channel_id + `",
-				"session_id": "2fb980f65e5c9a77c96ca01f2c242cf6",
-				"data": {
-					"version": "1077969938624553050",
+	if args[0] != prefix {
+		return
+	}
+	if args[1] == "imagine" {
+		prompt := "Batman"
+		jsonStr := `{
+			"type": 2,
+			"application_id": "936929561302675456",
+			"guild_id": "` + server_id + `",
+			"channel_id": "` + channel_id + `",
+			"session_id": "2fb980f65e5c9a77c96ca01f2c242cf6",
+			"data": {
+				"version": "1077969938624553050",
+				"id": "938956540159881230",
+				"name": "imagine",
+				"type": 1,
+				"options": [{
+					"type": 3,
+					"name": "prompt",
+					"value": "` + prompt + `"
+				}],
+				"application_command": {
 					"id": "938956540159881230",
-					"name": "imagine",
+					"application_id": "936929561302675456",
+					"version": "1077969938624553050",
+					"default_permission": true,
+					"default_member_permissions": null,
 					"type": 1,
+					"nsfw": false,
+					"name": "imagine",
+					"description": "Create images with Midjourney",
+					"dm_permission": true,
 					"options": [{
 						"type": 3,
 						"name": "prompt",
-						"value": "` + prompt + `"
-					}],
-					"application_command": {
-						"id": "938956540159881230",
-						"application_id": "936929561302675456",
-						"version": "1077969938624553050",
-						"default_permission": true,
-						"default_member_permissions": null,
-						"type": 1,
-						"nsfw": false,
-						"name": "imagine",
-						"description": "Create images with Midjourney",
-						"dm_permission": true,
-						"options": [{
-							"type": 3,
-							"name": "prompt",
-							"description": "The prompt to imagine",
-							"required": true
-						}]
-					},
-					"attachments": []
-				}
-			}`
-			fmt.Println(jsonStr)
-			req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
-			if err != nil {
-				log.Fatal(err)
+						"description": "The prompt to imagine",
+						"required": true
+					}]
+				},
+				"attachments": []
 			}
-			req.Header.Set("authorization", user_token)
-			req.Header.Set("Content-Type", "application/json")
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-			defer resp.Body.Close()
-			reqbody, _ := io.ReadAll(req.Body)
-
-			fmt.Println("request Body:", reqbody)
-			fmt.Println("response Status:", resp.Status)
-			fmt.Println("response Headers:", resp.Header)
-			body, _ := io.ReadAll(resp.Body)
-			fmt.Println("response Body:", string(body))
+		}`
+		fmt.Println(jsonStr)
+		req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
+		if err != nil {
+			log.Fatal(err)
 		}
-	})
+		req.Header.Set("authorization", user_token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))
+	}
+}
+func upscale(s *discordgo.Session, m *discordgo.MessageCreate) {
+	url := "https://discord.com/api/v9/interactions"
+	server_id := os.Getenv("SERVER_ID")
+	user_token := os.Getenv("USER_TOKEN")
+	channel_id := os.Getenv("CHANNEL_ID")
+
+	jsonStr := `{
+		"type": 3,
+		"guild_id": "` + server_id + `",
+		"channel_id": "` + channel_id + `",
+		"message_flags": 0,
+		"message_id": messageId,
+		"application_id": "936929561302675456",
+		"session_id": "45bc04dd4da37141a5f73dfbfaf5bdcf",
+		"data": {
+			"component_type": 2,
+			"custom_id": "MJ::JOB::upsample::{}::{}".format(index, messageHash)
+		}
+	}`
+
+	fmt.Println(jsonStr)
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authorization", user_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+}
+func maxUpscale(s *discordgo.Session, m *discordgo.MessageCreate) {
+	url := "https://discord.com/api/v9/interactions"
+	server_id := os.Getenv("SERVER_ID")
+	user_token := os.Getenv("USER_TOKEN")
+	channel_id := os.Getenv("CHANNEL_ID")
+
+	jsonStr := `{
+		"type": 3,
+		"guild_id": "` + server_id + `",
+		"channel_id": "` + channel_id + `",
+		"message_flags": 0,
+		"message_id": messageId,
+		"application_id": "936929561302675456",
+		"session_id": "1f3dbdf09efdf93d81a3a6420882c92c",
+		"data": {
+			"component_type": 2,
+			"custom_id": "MJ::JOB::upsample_max::1::{}::SOLO".format(messageHash)
+		}
+	}`
+
+	fmt.Println(jsonStr)
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authorization", user_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+}
+
+func variation(s *discordgo.Session, m *discordgo.MessageCreate) {
+	url := "https://discord.com/api/v9/interactions"
+	server_id := os.Getenv("SERVER_ID")
+	user_token := os.Getenv("USER_TOKEN")
+	channel_id := os.Getenv("CHANNEL_ID")
+
+	jsonStr := `{
+		"type": 3,
+		"guild_id": "` + server_id + `",
+		"channel_id": "` + channel_id + `",
+		"message_flags": 0,
+		"message_id": messageId,
+		"application_id": "936929561302675456",
+		"session_id": "1f3dbdf09efdf93d81a3a6420882c92c",
+		"data": {
+			"component_type": 2,
+			"custom_id": "MJ::JOB::variation::{}::{}".format(index, messageHash)
+		}
+	}`
+
+	fmt.Println(jsonStr)
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authorization", user_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 }
