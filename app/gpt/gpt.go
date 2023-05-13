0501d3cac60912073907f750f3f357d3c9f2eafa
inciner8r <punarvpawade17@gmail.com>
2023-05-13 16:24:34 +0530

    init: chatgpt integration for prompt

---
+++ app/gpt/gpt.go
@@ -0,0 +1,33 @@
+package gpt
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func GptRequest() {
+	apiKey := os.Getenv("API_KEY")
+	client := openai.NewClient(apiKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -39,87 +38,19 @@ func AddHandlers(sess *discordgo.Session) {
 	}
 	commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"generate": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			// Access options in the order provided by the user.
 			options := i.ApplicationCommandData().Options
 
-			// Or convert the slice into a map
 			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
 			for _, opt := range options {
 				optionMap[opt.Name] = opt
 			}
 
-			// This example stores the provided arguments in an []interface{}
-			// which will be used to format the bot's response
 			margs := make([]string, 0, len(options))
 
-			// msgformat := "You learned how to use command options! " +
-			// 	"Take a look at the value(s) you entered:\n"
-
-			// Get the value from the option map.
-			// When the option exists, ok = true
 			if option, ok := optionMap["prompt"]; ok {
-				// Option values must be type asserted from interface{}.
-				// Discordgo provides utility functions to make this simple.
 				margs = append(margs, option.StringValue())
 				prompt := strings.Join(margs[:], " ")
-				fmt.Println(prompt)
-
-				// msgformat += "> string-option: %s\n"
-				url := "https://discord.com/api/v9/interactions"
-				server_id := os.Getenv("SERVER_ID")
-				user_token := os.Getenv("USER_TOKEN")
-				channel_id := os.Getenv("CHANNEL_ID")
-				// 		prompt := "Batman"
-				jsonStr := `{
-					"type": 2,
-					"application_id": "936929561302675456",
-					"guild_id": "` + server_id + `",
-					"channel_id": "` + channel_id + `",
-					"session_id": "2fb980f65e5c9a77c96ca01f2c242cf6",
-					"data": {
-						"version": "1077969938624553050",
-						"id": "938956540159881230",
-						"name": "imagine",
-						"type": 1,
-						"options": [{
-							"type": 3,
-							"name": "prompt",
-							"value": "` + "wait" + `"
-						}],
-						"application_command": {
-							"id": "938956540159881230",
-							"application_id": "936929561302675456",
-							"version": "1077969938624553050",
-							"default_permission": true,
-							"default_member_permissions": null,
-							"type": 1,
-							"nsfw": false,
-							"name": "imagine",
-							"description": "Create images with Midjourney",
-							"dm_permission": true,
-							"options": [{
-								"type": 3,
-								"name": "prompt",
-								"description": "The prompt to imagine",
-								"required": true
-							}]
-						},
-						"attachments": []
-					}
-				}`
-				req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
-				if err != nil {
-					log.Fatal(err)
-				}
-				req.Header.Set("authorization", user_token)
-				req.Header.Set("Content-Type", "application/json")
-
-				client := &http.Client{}
-				resp, err := client.Do(req)
-				if err != nil {
-					panic(err)
-				}
-				defer resp.Body.Close()
+				generate(prompt)
 			}
 		},
 	}
@@ -132,6 +63,65 @@ func AddHandlers(sess *discordgo.Session) {
 	// sess.AddHandler(upscale)
 	// sess.AddHandler(maxUpscale)
 	// sess.AddHandler(variation)
+}
+
+func generate(prompt string) {
+	url := "https://discord.com/api/v9/interactions"
+	server_id := os.Getenv("SERVER_ID")
+	user_token := os.Getenv("USER_TOKEN")
+	channel_id := os.Getenv("CHANNEL_ID")
+	// 		prompt := "Batman"
+	jsonStr := `{
+		"type": 2,
+		"application_id": "936929561302675456",
+		"guild_id": "` + server_id + `",
+		"channel_id": "` + channel_id + `",
+		"session_id": "2fb980f65e5c9a77c96ca01f2c242cf6",
+		"data": {
+			"version": "1077969938624553050",
+			"id": "938956540159881230",
+			"name": "imagine",
+			"type": 1,
+			"options": [{
+				"type": 3,
+				"name": "prompt",
+				"value": "` + "wait" + `"
+			}],
+			"application_command": {
+				"id": "938956540159881230",
+				"application_id": "936929561302675456",
+				"version": "1077969938624553050",
+				"default_permission": true,
+				"default_member_permissions": null,
+				"type": 1,
+				"nsfw": false,
+				"name": "imagine",
+				"description": "Create images with Midjourney",
+				"dm_permission": true,
+				"options": [{
+					"type": 3,
+					"name": "prompt",
+					"description": "The prompt to imagine",
+					"required": true
+				}]
+			},
+			"attachments": []
+		}
+	}`
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(jsonStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authorization", user_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 }
 
 // func genImage(s *discordgo.Session, i *discordgo.InteractionCreate) {
